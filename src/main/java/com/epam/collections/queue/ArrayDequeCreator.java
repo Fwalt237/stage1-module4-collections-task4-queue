b11302bf2a77bc927dcd21a5ca7bf60c58354b77
Fwalt237 <patrickkamwa17@gmail.com>
2025-04-08 01:35:05 +0100

    queue

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,37 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> combinedDeque = new ArrayDeque<>();
+
+        if (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            combinedDeque.offerLast(firstQueue.poll());
+        }
+        if (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            combinedDeque.offerLast(firstQueue.poll());
+        }
+        if (!secondQueue.isEmpty() && !firstQueue.isEmpty()) {
+            combinedDeque.offerLast(secondQueue.poll());
+        }
+        if (!secondQueue.isEmpty() && !firstQueue.isEmpty()) {
+            combinedDeque.offerLast(secondQueue.poll());
+        }
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            verifyEmpty(firstQueue, combinedDeque);
+            verifyEmpty(secondQueue, combinedDeque);
+        }
+        return combinedDeque;
+    }
+
+    private void verifyEmpty(Queue<Integer> secondQueue, ArrayDeque<Integer> combinedDeque) {
+        if (!combinedDeque.isEmpty() && !secondQueue.isEmpty()) {
+            secondQueue.offer(combinedDeque.pollFirst());
+        }
+        if (!secondQueue.isEmpty()) {
+            combinedDeque.offerLast(secondQueue.poll());
+        }
+        if (!secondQueue.isEmpty()) {
+            combinedDeque.offerLast(secondQueue.poll());
+        }
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.offer(i);
+        }
+        List<Integer> dishOrder = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int k =1;
+            while(k < everyDishNumberToEat) {
+                if(!queue.isEmpty()) {
+                    queue.offer(queue.poll());
+                    k++;
+                } else{
+                    break;
+                }
+            }
+            dishOrder.add(queue.poll());
+        }
+        return dishOrder;
     }
 }
+
